9fd3fc86403ea0bdc80b96318f8895a079677385
katejank79 <katejank@seznam.cz>
2024-04-25 19:56:41 +0200

    Code for first and second exercise

---
+++ exerc1/challenge.js
@@ -0,0 +1,31 @@
+/*
+Values and variables
+Declare variables called country, continent and population and assign their values according
+to your own country (population in millions).
+    Log their values into console
+*/
+
+const country = 'Czech republic';
+const continent = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/*
+Data types
+Declare a variable called isIsland and set its value according to your country.
+The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+Log the types of isIsland, population, country and language to console
+*/
+
+let isIsland= false;
+let language;
+
+console.log(isIsland);
+console.log(language);
+
+
+
+
