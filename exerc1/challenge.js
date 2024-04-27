272cb93cbf5808fd0373ea7aa2df24e0826c1fc6
katejank79 <katejank@seznam.cz>
2024-04-27 18:15:50 +0200

    Code for first and second exercise

--- exerc1/challenge.js
+++ exerc1/challenge.js
@@ -25,7 +25,58 @@ let language;
 
 console.log(isIsland);
 console.log(language);
+console.log(country);
+console.log(continent);
+console.log(population);
 
+/*
+Set the value of language to the language spoken where you live (some countries have multiple languages,
+but just choose one).
+ */
+language = 'Czech';
 
+console.log(language);
+
+/*
+Think about which variables should be const variables (which values will never change, and which might change?).
+Then, change these variables to const.
+ */
+
+const language = 'Czech';
+console.log(language);
+
+/*Try to change one of the changed variables now, and observe what happens.
+*/
+
+/*
+Basic operators
+If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+ */
+const halfPopulation = population / 2;
+console.log(halfPopulation);
+
+/*
+Increase the population of your country by 1 and log the result to the console.
+ */
+population += 1;
+console.log(population);
+
+/*
+Finland has a population of 6 million. Does your country have more people than Finland?
+ */
+finlandPopulation = 6;
+console.log(finlandPopulation);
+
+/*
+The average population of a country is 33 million people.
+Does you country have less people than the average country?
+ */
+const averagePopulation = 33;
+
+/*
+ Based on the variables you created, create a new variable description which contains a string
+ with this format: 'Portugal is in Europe, and its 11 million people speak Portuguese'.
+ */
 
 
